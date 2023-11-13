07f427df20dd898e9fa2bf4f387259a5172f7ef3
ewewukek <ewewukek@gmail.com>
2023-11-13 12:51:33 +0300

    refactoring

--- src/addon_d.ipf/quickslottweak/quickslottweak.lua
+++ src/addon_d.ipf/quickslottweak/quickslottweak.lua
@@ -3,43 +3,26 @@ local acutil = require('acutil')
 function QUICKSLOTTWEAK_ON_INIT (addon, frame)
 	local frm = ui.GetFrame("joystickquickslot");
 
-	local ch = frm:GetChild("L2R2");
-	ch:ShowWindow(0);
+	frm:GetChild("L2R2"):ShowWindow(0);
+	frm:GetChild("refreshBtn"):ShowWindow(0);
 
-	ch = frm:GetChild("refreshBtn");
-	ch:ShowWindow(0);
+    frm:GetChildRecursively("L1_slot_Set1"):SetMargin(135, 0, 0, 10);
+    frm:GetChildRecursively("L2_slot_Set1"):SetMargin(0, 0, 0, 10);
+    frm:GetChildRecursively("R1_slot_Set1"):SetMargin(405, 0, 0, 10);
+    frm:GetChildRecursively("R2_slot_Set1"):SetMargin(540, 0, 0, 10);
 
-    ch = frm:GetChildRecursively("L1_slot_Set1");
-    ch:SetMargin(135, 0, 0, 10);
-    ch = frm:GetChildRecursively("L2_slot_Set1");
-    ch:SetMargin(0, 0, 0, 10);
-
-    ch = frm:GetChildRecursively("L1_slot_Set2");
-    ch:SetMargin(135, 0, 0, 10);
-    ch = frm:GetChildRecursively("L2_slot_Set2");
-    ch:SetMargin(0, 0, 0, 10);
-
-    ch = frm:GetChildRecursively("R1_slot_Set1");
-    ch:SetMargin(405, 0, 0, 10);
-    ch = frm:GetChildRecursively("R2_slot_Set1");
-    ch:SetMargin(540, 0, 0, 10);
-
-    ch = frm:GetChildRecursively("R1_slot_Set2");
-    ch:SetMargin(405, 0, 0, 10);
-    ch = frm:GetChildRecursively("R2_slot_Set2");
-    ch:SetMargin(540, 0, 0, 10);
+    frm:GetChildRecursively("L1_slot_Set2"):SetMargin(135, 0, 0, 10);
+    frm:GetChildRecursively("L2_slot_Set2"):SetMargin(0, 0, 0, 10);
+    frm:GetChildRecursively("R1_slot_Set2"):SetMargin(405, 0, 0, 10);
+    frm:GetChildRecursively("R2_slot_Set2"):SetMargin(540, 0, 0, 10);
 
     frm = ui.GetFrame("joystickrestquickslot");
 
-    ch = frm:GetChildRecursively("L1_slot_Set1");
-    ch:SetMargin(135, 0, 0, 0);
-    ch = frm:GetChildRecursively("L2_slot_Set1");
-    ch:SetMargin(0, 0, 0, 0);
+    frm:GetChildRecursively("L1_slot_Set1"):SetMargin(135, 0, 0, 0);
+    frm:GetChildRecursively("L2_slot_Set1"):SetMargin(0, 0, 0, 0);
 
-    ch = frm:GetChildRecursively("R1_slot_Set1");
-    ch:SetMargin(405, 0, 0, 0);
-    ch = frm:GetChildRecursively("R2_slot_Set1");
-    ch:SetMargin(540, 0, 0, 0);
+    frm:GetChildRecursively("R1_slot_Set1"):SetMargin(405, 0, 0, 0);
+    frm:GetChildRecursively("R2_slot_Set1"):SetMargin(540, 0, 0, 0);
 
     acutil.log("Quick slot tweak loaded!")
 end
